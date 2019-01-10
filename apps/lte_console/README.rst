c2acd21ae1d90eeb4bbbfdea505195b4f3efa1d4
Ryan Erickson <ryan.erickson@lairdtech.com>
2019-01-10 12:00:58 -0600

    lte_console: update to use HL7800 driver
    lte_console provides a command line interface to test the HL7800 driver.
    Remove all bluetooth functionality.
    Add HL commands to control the HL7800.

--- apps/lte_console/README.rst
+++ apps/lte_console/README.rst
@@ -1,25 +1,20 @@
-.. _ble_peripheral:
+.. _sirrus100_lte_console:
 
-Bluetooth: Peripheral
+Sirrus 100: LTE Console
 #####################
 
 Overview
 ********
 
-Application demonstrating the BLE Peripheral role. It has several well-known and
-vendor-specific GATT services that it exposes.
+TODO
 
 
 Requirements
 ************
 
-* BlueZ running on the host, or
-* A board with BLE support
+TODO
 
 Building and Running
 ********************
 
-This sample can be found under :file:`samples/bluetooth/peripheral` in the
-Zephyr tree.
-
-See :ref:`bluetooth setup section <bluetooth_setup>` for details.
+TODO
--- apps/lte_console/src/main.c
+++ apps/lte_console/src/main.c
@@ -3,8 +3,62 @@
 #include <zephyr.h>
 #include <misc/printk.h>
 #include <version.h>
+#include <gpio.h>
+#include <shell/shell.h>
+#include <shell/shell_uart.h>
+#include <drivers/modem/hl7800.h>
 
 #include "app_led.h"
+
+#define HEARTBEAT_INTERVAL K_SECONDS(2)
+
+static int hl7800_pwr_off(const struct shell *shell, size_t argc, char **argv)
+{
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+
+    mdm_hl7800_power_off();
+    return 0;
+}
+
+static int hl7800_reset(const struct shell *shell, size_t argc, char **argv)
+{
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+
+    mdm_hl7800_reset();
+
+    return 0;
+}
+
+static int hl7800_wake(const struct shell *shell, size_t argc, char **argv)
+{
+    ARG_UNUSED(argc);
+
+    int val = atoi(argv[1]);
+    mdm_hl7800_wakeup(val);
+
+    return 0;
+}
+
+static int hl7800_send_at_cmd(const struct shell *shell, size_t argc, char **argv)
+{
+    ARG_UNUSED(argc);
+
+    const u8_t *cmd = argv[1];
+    mdm_hl7800_send_at_cmd(cmd);
+
+    return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(hl_cmds,
+                               SHELL_CMD(pwr_off, NULL, "Power off HL7800", hl7800_pwr_off),
+                               SHELL_CMD(reset, NULL, "Reset HL7800", hl7800_reset),
+                               SHELL_CMD_ARG(wake, NULL, "Wake/Sleep HL7800", hl7800_wake, 2, 0),
+                               SHELL_CMD_ARG(send, NULL, "Send AT cmd to HL7800", hl7800_send_at_cmd, 2, 0),
+                               SHELL_SUBCMD_SET_END /* Array terminated. */
+);
+SHELL_CMD_REGISTER(hl, &hl_cmds, "HL7800 commands", NULL);
 
 void main(void)
 {
@@ -18,7 +72,7 @@ void main(void)
         // TODO: LED heartbeat should use a timer instead of relying on the thread loop.
         // TODO: wait in main thread on event (semaphore) until some work needs to be done.
         ledHeartBeat();
-        k_sleep(MSEC_PER_SEC);
-        //printk("Heartbeat!\n");
+
+        k_sleep(HEARTBEAT_INTERVAL);
     }
 }
