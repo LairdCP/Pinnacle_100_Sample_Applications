697fefbd4f8b39f0fa31a38f686dd0c70f998094
Ryan Erickson <ryan.erickson@lairdtech.com>
2019-03-13 10:38:11 -0500

    lte_console: cleanup
    Cleanup cmakelists file
    cleanup proj.conf
    Cleanup formatting in main.c

--- apps/lte_console/src/main.c
+++ apps/lte_console/src/main.c
@@ -14,65 +14,62 @@
 
 static int hl7800_pwr_off(const struct shell *shell, size_t argc, char **argv)
 {
-    ARG_UNUSED(argc);
-    ARG_UNUSED(argv);
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
 
-    mdm_hl7800_power_off();
-    return 0;
+	return mdm_hl7800_power_off();
 }
 
 static int hl7800_reset(const struct shell *shell, size_t argc, char **argv)
 {
-    ARG_UNUSED(argc);
-    ARG_UNUSED(argv);
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
 
-    mdm_hl7800_reset();
-
-    return 0;
+	return mdm_hl7800_reset();
 }
 
 static int hl7800_wake(const struct shell *shell, size_t argc, char **argv)
 {
-    ARG_UNUSED(argc);
+	ARG_UNUSED(argc);
 
-    int val = atoi(argv[1]);
-    mdm_hl7800_wakeup(val);
+	int val = atoi(argv[1]);
+	mdm_hl7800_wakeup(val);
 
-    return 0;
+	return 0;
 }
 
-static int hl7800_send_at_cmd(const struct shell *shell, size_t argc, char **argv)
+static int hl7800_send_at_cmd(const struct shell *shell, size_t argc,
+			      char **argv)
 {
-    ARG_UNUSED(argc);
+	ARG_UNUSED(argc);
 
-    const u8_t *cmd = argv[1];
-    mdm_hl7800_send_at_cmd(cmd);
+	const u8_t *cmd = argv[1];
 
-    return 0;
+	return mdm_hl7800_send_at_cmd(cmd);
 }
 
-SHELL_STATIC_SUBCMD_SET_CREATE(hl_cmds,
-                               SHELL_CMD(pwr_off, NULL, "Power off HL7800", hl7800_pwr_off),
-                               SHELL_CMD(reset, NULL, "Reset HL7800", hl7800_reset),
-                               SHELL_CMD_ARG(wake, NULL, "Wake/Sleep HL7800", hl7800_wake, 2, 0),
-                               SHELL_CMD_ARG(send, NULL, "Send AT cmd to HL7800", hl7800_send_at_cmd, 2, 0),
-                               SHELL_SUBCMD_SET_END /* Array terminated. */
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	hl_cmds, SHELL_CMD(pwr_off, NULL, "Power off HL7800", hl7800_pwr_off),
+	SHELL_CMD(reset, NULL, "Reset HL7800", hl7800_reset),
+	SHELL_CMD_ARG(wake, NULL, "Wake/Sleep HL7800", hl7800_wake, 2, 0),
+	SHELL_CMD_ARG(send, NULL, "Send AT cmd to HL7800", hl7800_send_at_cmd,
+		      2, 0),
+	SHELL_SUBCMD_SET_END /* Array terminated. */
 );
 SHELL_CMD_REGISTER(hl, &hl_cmds, "HL7800 commands", NULL);
 
 void main(void)
 {
-    printk("Sirrus 100 LTE Console %d.%d.%d\nBranch: %s\nCommit: %s\nSystem up time %d us\n\n",
-           APP_MAJOR, APP_MINOR, APP_PATCH,
-           GIT_BRANCH, GIT_COMMIT_HASH, SYS_CLOCK_HW_CYCLES_TO_NS(k_cycle_get_32()) / 1000);
-    led_init();
+	printk("Sirrus 100 LTE Console %d.%d.%d\r\nBranch: %s\r\nCommit: %s\r\nSystem up time %d us\r\n\r\n",
+	       APP_MAJOR, APP_MINOR, APP_PATCH, GIT_BRANCH, GIT_COMMIT_HASH,
+	       SYS_CLOCK_HW_CYCLES_TO_NS(k_cycle_get_32()) / 1000);
+	led_init();
 
-    while (1)
-    {
-        // TODO: LED heartbeat should use a timer instead of relying on the thread loop.
-        // TODO: wait in main thread on event (semaphore) until some work needs to be done.
-        ledHeartBeat();
+	while (1) {
+		// TODO: LED heartbeat should use a timer instead of relying on the thread loop.
+		// TODO: wait in main thread on event (semaphore) until some work needs to be done.
+		ledHeartBeat();
 
-        k_sleep(HEARTBEAT_INTERVAL);
-    }
+		k_sleep(HEARTBEAT_INTERVAL);
+	}
 }
