dbd3525524bb4455b732ea2486ab32aa39d45b2f
thedjnK <spam@helper3000.net>
2019-12-16 14:40:57 +0000

    Add extended bootloader function

--- apps/regression_test/src/main.c
+++ apps/regression_test/src/main.c
@@ -120,10 +120,98 @@ static int ubldr_get(const struct shell *shell, size_t argc, char **argv)
     return 0;
 }
 
+static int ubldr_getb(const struct shell *shell, size_t argc, char **argv)
+{
+    ARG_UNUSED(argc);
+
+    const u8_t *cmd = argv[1];
+    u8_t nPartition = strtoul(argv[2], NULL, 16);
+
+    uint32_t nCmdID = strtoul(cmd, NULL, 16);
+
+    bootloader_function_get_t bldrload = (bootloader_function_get_t)BOOTLOADER_STORAGE_FUNCTION_GET_ADDRESS;
+
+    uint16_t nFlags = 0;
+    uint32_t nDataLen = 0;
+    uint32_t resCode;
+    resCode = bldrload(nCmdID, nPartition, 0, NULL, 0, &nDataLen, &nFlags);
+
+    printk("  Partition: %d, Ret: %d, Len: %d, Flg: %x, ", nPartition, resCode, nDataLen, nFlags);
+    if (argc > 3)
+    {
+        const u8_t *cmd = argv[3];
+        if (cmd[0] == DISPLAY_NO_DATA)
+        {
+            printk("\r\n");
+            return 0;
+        }
+    }
+
+    if (resCode == 0)
+    {
+        uint8_t pData[nDataLen+1];
+        uint8_t pDataDupe[(nDataLen*2)+1];
+        resCode = bldrload(nCmdID, nPartition, 0, pData, nDataLen, NULL, NULL);
+
+        pDataDupe[0] = 0;
+        if (pData != NULL)
+        {
+            uint8_t i = 0;
+            if (argc > 3)
+            {
+                const u8_t *cmd = argv[3];
+                if (cmd[0] == DISPLAY_ASCII)
+                {
+                    printk("Data: %s", pData);
+                }
+                else
+                {
+                    while (i < nDataLen)
+                    {
+                        if (cmd[0] == DISPLAY_HEX_BYTE_ENDIAN_FLIP)
+                        {
+                            sprintf(pDataDupe, "%s%02X%02X", pDataDupe, pData[i+1], pData[i]);
+                        }
+                        else if (cmd[0] == DISPLAY_HEX_QUAD_ENDIAN_FLIP)
+                        {
+                            sprintf(pDataDupe, "%s%02X%02X", pDataDupe, pData[nDataLen-i-1], pData[nDataLen-i-2]);
+                        }
+                        else
+                        {
+                            sprintf(pDataDupe, "%s%02X%02X", pDataDupe, pData[i], pData[i+1]);
+                        }
+
+                        i += 2;
+                    }
+                    printk("Data: %s", pDataDupe);
+                }
+            }
+            else
+            {
+                while (i < nDataLen)
+                {
+                    sprintf(pDataDupe, "%s%02X%02X", pDataDupe, pData[i], pData[i+1]);
+                    i += 2;
+                }
+                printk("Data: %s", pDataDupe);
+            }
+        }
+        else
+        {
+            printk("failed.");
+        }
+    }
+    printk("  \r\n");
+
+    return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(
     ubldr_cmds, SHELL_CMD(boot, NULL, "Boot into universal bootloader", ubldr_boot),
     SHELL_CMD_ARG(get, NULL, "Get universal bootloader info", ubldr_get,
                   2, 1),
+    SHELL_CMD_ARG(getb, NULL, "Get extended universal bootloader info", ubldr_getb,
+                  3, 1),
     SHELL_SUBCMD_SET_END /* Array terminated. */
 );
 SHELL_CMD_REGISTER(ubldr, &ubldr_cmds, "Universal Bootloader commands", NULL);
